eed02999ae5287b1db46c7efd613a55583aad6c5
dbrazl <danielbrazlopes16@gmail.com>
2021-05-02 23:04:11 -0300

    fix: remove menu persitation

--- src/store/persistReducers.js
+++ src/store/persistReducers.js
@@ -6,7 +6,7 @@ export default reducers => {
     {
       key: 'mais-med',
       storage,
-      whitelist: ['menu', 'auth', 'user'],
+      whitelist: ['auth', 'user'],
     },
     reducers
   );
